a408f13bb17d9088bd9e0f83b0e0c8b1ec941045
Koura <tomppa.j@hotmail.com>
2013-04-24 20:07:09 +0300

    DX10Model implementation

---
+++ Yarr_sharp_eyes/DX10Model.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Model.h"
+#include <d3d10.h>
+#include <d3dx10math.h>
+
+class DX10Model : public Model
+{
+private:
+	struct VertexType
+	{
+		D3DXVECTOR3 position;
+		D3DXVECTOR4 color;
+	};
+public:
+	DX10Model();
+	DX10Model(const DX10Model&);
+	~DX10Model();
+	bool Initialize(char*, ID3D10Device*, float, float, float, float);
+	void Shutdown();
+	void Render(ID3D10Device*);
+private: 
+	bool InitializeBuffers(ID3D10Device*, float, float, float , float);
+	void ShutdownBuffers();
+	void RenderBuffers(ID3D10Device*);
+private:
+	ID3D10Buffer *m_vertexBuffer, *m_indexBuffer;
+};
