f5d93e9c0316f21f957b58a962d5f9f92cd1cc71
Olavi Lintumäki <atomimalli@gmail.com>
2013-05-15 15:53:29 +0300

    add fog

--- Game/update.lua
+++ Game/update.lua
@@ -3,9 +3,9 @@ function update()
 	frames=frames+1
 	print("frames: " .. frames)
 	
-	if frames % 100 == 1 then 
+	if frames % 10 == 1 then 
 		local j=scene:NewEntity()
-		j:SetPosition(math.sin(frames)*5,math.cos(frames)*10,3+math.sin(frames*7)*3)
+		j:SetPosition(math.sin(frames)*10,math.cos(frames)*5+5,3+math.sin(frames*7)*3)
 	end
 end
 
