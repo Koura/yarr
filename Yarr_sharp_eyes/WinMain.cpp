792dc43de474dc2a643f8a00e1e685f1ad9399c4
Koura <tomppa.j@hotmail.com>
2013-05-15 10:22:43 +0300

    added console to yarr_sharp_eyes project

--- Yarr_sharp_eyes/WinMain.cpp
+++ Yarr_sharp_eyes/WinMain.cpp
@@ -1,6 +1,9 @@
 #include "CoreSystem.h"
 #include "Window32.h"
 #include "D3D10Renderer.h"
+#include <stdio.h>
+#include <io.h>
+#include <fcntl.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdShow)
 { 
@@ -13,6 +16,19 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pScmdline,
 	int screenWidth, screenHeight;
 	screenWidth = 800;
 	screenHeight = 600;
+	 AllocConsole();
+
+    HANDLE handle_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    int hCrt = _open_osfhandle((long) handle_out, _O_TEXT);
+    FILE* hf_out = _fdopen(hCrt, "w");
+    setvbuf(hf_out, NULL, _IONBF, 1);
+    *stdout = *hf_out;
+
+    HANDLE handle_in = GetStdHandle(STD_INPUT_HANDLE);
+    hCrt = _open_osfhandle((long) handle_in, _O_TEXT);
+    FILE* hf_in = _fdopen(hCrt, "r");
+    setvbuf(hf_in, NULL, _IONBF, 128);
+    *stdin = *hf_in;
 	if(!System)
 	{
 		return 1;
